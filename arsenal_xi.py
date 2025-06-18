1d82d1439c5ac1ef2cab7343145b939c76fafdfd
SuperjohnaCreates <mrjohnotieno@gmail.com>
2025-06-18 03:12:04 +0300

    Initial lineup with Smith Rowe

---
+++ arsenal_xi.py
@@ -0,0 +1,9 @@
+starting_eleven = [
+    "Ramsdale", "White", "Saliba", "Gabriel", "Zinchenko",
+    "Rice", "Odegaard", "Smith Rowe",
+    "Saka", "Jesus", "Martinelli"
+]
+
+print("Today's Arsenal Starting XI:")
+for player in starting_eleven:
+    print(player)
